b69136b07f71fc9472e9300f9e4f70d2819e99d9
GhtGhoster <32255934+GhtGhoster@users.noreply.github.com>
2023-05-19 17:21:51 +0200

    Improve code structure

--- src/hex_matrix.rs
+++ src/hex_matrix.rs
@@ -1,13 +1,40 @@
 
+use macroquad::prelude::{*};
+
 // hex_horizontal
-const NEIGHBOUR_INDEX_DELTAS: [(isize, isize); 6] = [
-    (0, 1),
+const NEIGHBOUR_INDEX_DELTAS_HORIZONTAL_EVEN: [(isize, isize); 6] = [
     (0, -1),
-    (-1, -1),
+    (0, 1),
     (-1, 0),
+    (-1, -1),
+    (1, 0),
+    (1, -1),
+];
+const NEIGHBOUR_INDEX_DELTAS_HORIZONTAL_ODD: [(isize, isize); 6] = [
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (-1, 1),
+    (1, 0),
+    (1, 1),
+];
+const NEIGHBOUR_INDEX_DELTAS_VERTICAL_EVEN: [(isize, isize); 6] = [
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (-1, -1),
+    (-1, 1),
+    (1, 0),
+];
+const NEIGHBOUR_INDEX_DELTAS_VERTICAL_ODD: [(isize, isize); 6] = [
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (1, 0),
     (1, -1),
     (1, 1),
 ];
+pub const HEX_CONST: f32 = 0.8660254; // 3f32.sqrt() / 2f32
 
 pub struct HexMatrix {
     pub hex_vertical: bool,
@@ -19,13 +46,8 @@ impl HexMatrix {
     pub fn new(hex_vertical: bool, matrix_size: (usize, usize)) -> Self {
         let (matrix_width, matrix_height): (usize, usize) = matrix_size;
         let mut matrix: Vec<Vec<f32>> = Vec::with_capacity(matrix_height);
-        for i in 0..matrix_height {
+        for _ in 0..matrix_height {
             matrix.push(vec![0.0; matrix_width]);
-            // matrix[i][matrix_width/2] = 6600.0;
-            // if i == matrix_height / 2 {
-            //     matrix.pop();
-            //     matrix.push(vec![6600.0; matrix_width]);
-            // }
         }
         HexMatrix {
             hex_vertical,
@@ -40,7 +62,20 @@ impl HexMatrix {
 
     pub fn neighbour_indices(&self, index: (isize, isize)) -> Vec<(usize, usize)> {
         let mut result: Vec<(usize, usize)> = vec![];
-        for (x_delta, y_delta) in NEIGHBOUR_INDEX_DELTAS {
+        let neighbour_index_deltas = if self.hex_vertical {
+            if index.1&1 == 0 {
+                NEIGHBOUR_INDEX_DELTAS_VERTICAL_EVEN
+            } else {
+                NEIGHBOUR_INDEX_DELTAS_VERTICAL_ODD
+            }
+        } else {
+            if index.0&1 == 0 {
+                NEIGHBOUR_INDEX_DELTAS_HORIZONTAL_EVEN
+            } else {
+                NEIGHBOUR_INDEX_DELTAS_HORIZONTAL_ODD
+            }
+        };
+        for (x_delta, y_delta) in neighbour_index_deltas {
             let neighbour_x: isize = index.0 + x_delta;
             let neighbour_y: isize = index.1 + y_delta;
             if self.contains_index((neighbour_x, neighbour_y)) {
@@ -49,4 +84,77 @@ impl HexMatrix {
         }
         result
     }
+
+    pub fn get_mouse_hex_coords(&self, hex_size: f32) -> (isize, isize) {
+        let (mut mouse_x, mut mouse_y): (f32, f32) = mouse_position();
+        if self.hex_vertical {
+            mouse_x -= HEX_CONST * hex_size;
+            mouse_y -= hex_size;
+        } else {
+            mouse_x -= hex_size;
+            mouse_y -= HEX_CONST * hex_size;
+        }
+        let (q, r): (f32, f32) = if self.hex_vertical {
+            (
+                (3f32.sqrt()/3.0 * mouse_x - 1.0/3.0 * mouse_y) / hex_size,
+                ((2.0/3.0) * mouse_y) / hex_size,
+            )
+        } else {
+            (
+                (2.0/3.0 * mouse_x) / hex_size,
+                (-1.0/3.0 * mouse_x + 3f32.sqrt()/3.0 * mouse_y) / hex_size,
+            )
+        };
+        let (q, r): (isize, isize) = cube_round((q, r));
+        let (x, y): (isize, isize) = if self.hex_vertical {
+            (
+                (q + (r - (r&1)) / 2) as isize,
+                r as isize,
+            )
+        } else {
+            (
+                q as isize,
+                (r + (q - (q&1)) / 2) as isize,
+            )
+        };
+        (x, y)
+    }
+    
+    pub fn offset_coord_to_center_pixel(&self, hex_size: f32, (i, j): (usize, usize)) -> (f32, f32) { 
+        if self.hex_vertical {
+            (
+                hex_size * HEX_CONST * ((j%2) + 1 + (2*i)) as f32,
+                hex_size * ((j as f32 * 1.5) + 1.0) as f32
+            )
+        } else {
+            (
+                hex_size * ((i as f32 * 1.5) + 1.0) as f32,
+                hex_size * HEX_CONST * ((i%2) + 1 + (2*j)) as f32
+            )
+        }
+    }
+}
+
+// https://www.redblobgames.com/grids/hexagons/#rounding
+pub fn cube_round((q, r): (f32, f32)) -> (isize, isize) {
+    let s = -q-r;
+
+    let mut rounded_q = q.round();
+    let mut rounded_r = r.round();
+    let rounded_s = s.round();
+
+    let q_diff = (rounded_q - q).abs();
+    let r_diff = (rounded_r - r).abs();
+    let s_diff = (rounded_s - s).abs();
+
+    if q_diff > r_diff && q_diff > s_diff {
+        rounded_q = -rounded_r-rounded_s;
+    } else if r_diff > s_diff {
+        rounded_r = -rounded_q-rounded_s;
+    }
+    // else {
+    //     rounded_s = -rounded_q-rounded_r;
+    // }
+
+    (rounded_q as isize, rounded_r as isize)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@
 use macroquad::prelude::{*};
 
 mod hex_matrix;
-use hex_matrix::HexMatrix;
-
-mod hex_logic;
-use hex_logic::get_mouse_hex_coords;
+use hex_matrix::{HexMatrix, HEX_CONST};
 
 mod color_temperature;
 use color_temperature::kelvin_to_color;
@@ -15,7 +12,6 @@ async fn main() {
     // debugging
     // std::env::set_var("RUST_BACKTRACE", "1");
 
-    let hex_const: f32 = f32::sqrt(3.0) / 2.0;
     let hex_size_min: f32 = 5.0;
     let hex_size_max: f32 = 50.0;
 
@@ -61,14 +57,14 @@ async fn main() {
         let new_matrix_size: (usize, usize) = if matrix.hex_vertical {
             let new_screen_width: f32 = screen_height() - (0.5 * hex_size);
             let new_matrix_width: usize = (new_screen_width / (1.5 * hex_size)).floor() as usize;
-            let new_screen_height: f32 = screen_width() - (hex_const * hex_size);
-            let new_matrix_height: usize = (new_screen_height / (hex_const * hex_size)).floor() as usize / 2;
+            let new_screen_height: f32 = screen_width() - (HEX_CONST * hex_size);
+            let new_matrix_height: usize = (new_screen_height / (HEX_CONST * hex_size)).floor() as usize / 2;
             (new_matrix_height, new_matrix_width)
         } else {
             let new_screen_width: f32 = screen_width() - (0.5 * hex_size);
             let new_matrix_width: usize = (new_screen_width / (1.5 * hex_size)).floor() as usize;
-            let new_screen_height: f32 = screen_height() - (hex_const * hex_size);
-            let new_matrix_height: usize = (new_screen_height / (hex_const * hex_size)).floor() as usize / 2;
+            let new_screen_height: f32 = screen_height() - (HEX_CONST * hex_size);
+            let new_matrix_height: usize = (new_screen_height / (HEX_CONST * hex_size)).floor() as usize / 2;
             (new_matrix_width, new_matrix_height)
         };
 
@@ -79,7 +75,7 @@ async fn main() {
         // automaton input
         if is_mouse_button_down(MouseButton::Left) || is_mouse_button_down(MouseButton::Right) {
             // TODO: improve sub-frame movement input handling (draw line between last and current mouse point)
-            let (x, y): (isize, isize) = get_mouse_hex_coords(hex_size, &matrix);
+            let (x, y): (isize, isize) = matrix.get_mouse_hex_coords(hex_size);
             if matrix.contains_index((x, y)) {
                 //println!("[{}, {}] -> click: ({}, {})", matrix.matrix_size.0, matrix.matrix_size.1, x, y);
                 let (x, y): (usize, usize) = (x as usize, y as usize);
@@ -105,17 +101,7 @@ async fn main() {
 
         for i in 0..matrix.matrix_size.0 {
             for j in 0..matrix.matrix_size.1 {
-                let (x, y): (f32, f32) = if matrix.hex_vertical {
-                    (
-                        hex_size * hex_const * ((j%2) + 1 + (2*i)) as f32,
-                        hex_size * ((j as f32 * 1.5) + 1.0) as f32
-                    )
-                } else {
-                    (
-                        hex_size * ((i as f32 * 1.5) + 1.0) as f32,
-                        hex_size * hex_const * ((i%2) + 1 + (2*j)) as f32
-                    )
-                };
+                let (x, y): (f32, f32) = matrix.offset_coord_to_center_pixel(hex_size, (i, j));
                 let color: Color = kelvin_to_color(matrix.matrix[j][i]);
                 draw_hexagon(
                     x, //screen_width()/2.0,
