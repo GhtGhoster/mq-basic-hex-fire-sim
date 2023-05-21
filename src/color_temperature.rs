c270a779b82c22f92eb9b28a957614c800d44c6b
GhtGhoster <32255934+GhtGhoster@users.noreply.github.com>
2023-05-21 12:09:39 +0200

    Update color_temperature.rs

--- src/color_temperature.rs
+++ src/color_temperature.rs
@@ -2,35 +2,34 @@
 use macroquad::prelude::*;
 
 pub fn kelvin_to_color(temperature: f32) -> Color {
-    // check if I can call powf on a float itself
     // edge case
-    if temperature <= 0f32 {
+    if temperature <= 0.0 {
         return BLACK;
     };
 
     // setup
-    let temp = temperature / 100f32;
+    let temp = temperature / 100.0;
 
-    let (mut r, mut g, mut b) = if temp <= 66f32 {
+    let (mut r, mut g, mut b): (f32, f32, f32) = if temp <= 66.0 {
         (
-            255f32,
-            99.4708025861 * f32::ln(temp) - 161.1195681661,
-            if temp <= 19f32 {
-                0f32
+            255.0,
+            99.4708025861 * temp.ln() - 161.1195681661,
+            if temp <= 19.0 {
+                0.0
             } else {
-                138.5177312231 * f32::ln(temp - 10f32) - 305.0447927307
-            }
+                138.5177312231 * (temp - 10.0).ln() - 305.0447927307
+            },
         )
     } else {
         (
-            329.698727466 * f32::powf(temp - 60f32, -0.1332047592),
-            288.1221695283 * f32::powf(temp - 60f32, -0.0755148492),
-            255f32
+            329.698727466 * (temp - 60.0).powf(-0.1332047592),
+            288.1221695283 * (temp - 60.0).powf(-0.0755148492),
+            255.0,
         )
     };
 
-    if temperature < 1000f32 {
-        let lightness = temperature/1000f32;
+    if temperature < 1000.0 {
+        let lightness = temperature/1000.0;
         r *= lightness;
         g *= lightness;
         b *= lightness;
